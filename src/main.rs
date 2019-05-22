03504569088925aa5b2f3fe9f8fc579fe9efd43b
shanPic <zhanshan333@outlook.com>
2019-05-22 19:15:28 +0800

    variables-and-mutability

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    // let x = 5; //error! rust的变量默认不可变
+    let mut x = 5;
+    println!("The value of x is {}", x);
+    x = 10;
+    println!("The value of x is {}", x);
 }
