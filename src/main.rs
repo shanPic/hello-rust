c53b5d5fc04e7033a1ae5f8fad1d72d78f82cc05
shanPic <zhanshan333@outlook.com>
2019-05-22 19:37:07 +0800

    shadowing with diffrent type

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ fn main() {
     // let x = 5; //error! rust的变量默认不可变
     let x = 5;
     println!("The value of x is {}", x);
-    let x = 15;
+    let x = "liuzhanshan";
     println!("The value of x is {}", x);
 }
