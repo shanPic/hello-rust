d41868ff63011fa0ed48096830e0ee6cf4e7688c
shanPic <zhanshan333@outlook.com>
2019-05-25 02:14:46 +0800

    ownership and borrow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let mut str = String::from("string");
+    change(&mut str);
+    println!("{}", str);
+    
+    let slice = first_word(&str);
+    println!("{}", slice);
+}
+
+fn change(str : &mut String) -> usize {
+    str.push_str("_push_str");
+    return str.len();
+}
+
+fn first_word(str : &str) -> &str {
+    return str;
 }
