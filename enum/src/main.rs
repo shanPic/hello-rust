9643e0952304e7d1f9c7d6da41d0ae048cf09d91
shanPic <zhanshan333@outlook.com>
2020-01-07 23:26:19 +0800

    change enum's sub-name to camel style.

--- enum/src/main.rs
+++ enum/src/main.rs
@@ -1,14 +1,14 @@
 enum Coin {
-    ten,
-    five(Option<i32>),
+    Ten,
+    Five(Option<i32>),
 }
 
 fn main() {
-    let one_coin = Coin::five(Some(1));
+    let one_coin = Coin::Five(Some(1));
     let mr = match one_coin {
-        Coin::ten => 1,
-        Coin::five(Some(1)) => 2,
-        Coin::five(None) => 3,
+        Coin::Ten => 1,
+        Coin::Five(Some(1)) => 2,
+        Coin::Five(None) => 3,
         _ => 4,
     };
     println!("{}", mr);
